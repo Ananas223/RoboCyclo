a85e021ef0ef18845b1541daff659a77c56b97e9
Aleskandr <aleksandrbabuskin702@gmail.com>
2025-05-18 00:33:37 +0300

    header file for CyclogramClass created

---
+++ include/CyclogramClass.h
@@ -0,0 +1,26 @@
+#include <vector>
+#include <map>
+#include <string>
+
+#pragma once
+
+//Структура описывающая одну точку в циклограмме
+struct Point {
+    std::map<std::string, double> cords; //Словарь степень свободы робота -> ее координата
+    double time; //Время текущей точки
+};
+
+class Cyclogram {
+private:
+
+    std::vector<Point> points;
+    std::vector<std::string> joints; //Вектор существующих степеней свободы
+    void checkJoints(); //Функция для проверки степени свободы на существование
+    void sortPoints(); //Функция для сортировки точек по времени
+
+public:
+
+    Cyclogram(std::vector<std::string> joints); //Конструктор, принимает вектор существующих степеней свободы
+    int addPoint(std::map<std::string, double> cords, time);
+
+};
