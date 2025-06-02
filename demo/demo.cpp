08a5f338d390d31475db1bf05ccb3448b1255645
Aleskandr <aleksandrbabuskin702@gmail.com>
2025-06-02 21:36:51 +0300

    Demo code updated

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -7,5 +7,18 @@ int main(){
     std::vector<std::string> jnt = {"Joint_1", "Joint_2", "Joint_3"};
     Cyclogram my_cycle(jnt);
 
+    // Словарь степень свободы -> текущая координата
+    std::map<std::string, double> new_p = {
+            {"Joint_1", 0.7},
+            {"Joint_2", 1.2},
+            {"Joint_3", 3.4}
+    };
+
+    // Добавления новой точки
+    my_cycle.addPoint(new_p, 0.5);
+
+    // Сохранение в CSV
+    my_cycle.saveCSV("test.csv");
+
     return 0;
 }
